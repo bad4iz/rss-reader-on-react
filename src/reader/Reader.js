4a0755e98b01eb87353102e2319f704a6dd44f9a
Евгений Плотников <bad4iz@gmail.com>
2017-06-14 10:05:39 +0400

    read

--- src/reader/Reader.js
+++ src/reader/Reader.js
@@ -1,3 +1,17 @@
 /**
  * Created by bad4iz on 14.06.2017.
  */
+import React, { Component } from 'react';
+
+class Reader extends Component {
+
+    render() {
+        return (
+            <div>
+                Reader
+            </div>
+        );
+    }
+}
+
+export default Reader;
